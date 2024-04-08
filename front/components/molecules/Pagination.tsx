db0f67899bf4e77f8001dc4bfc7c0f4852123216
j.kashiwagi <jun.1016.a.b.c.d.e@gmail.com>
2024-04-09 04:47:29 +0900

    ページネーションCSS調整

--- front/components/molecules/Pagination.tsx
+++ front/components/molecules/Pagination.tsx
@@ -19,25 +19,25 @@ const Pagination = ({
   sizePerPage: number;
   currentPage: number;
   path: string;
-  }) => {
+}) => {
   const totalPage = Math.ceil(total / sizePerPage);
   return (
     <div className="flex items-center justify-between border-t border-gray-200 bg-white px-4 py-8 sm:px-6">
       <div className="flex flex-1 items-center justify-center">
         <div>
           <nav
-            className="isolate inline-flex -space-x-px rounded-md shadow-sm"
+            className="isolate inline-flex -space-x-px gap-x-4"
             aria-label="Pagination"
           >
             <Link
               href={`${path}/1`}
-              className="relative inline-flex items-center rounded-l-md px-2 py-2 text-gray-400 ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus:z-20 focus:outline-offset-0"
+              className="relative inline-flex items-center px-2 py-2 text-gray-800 hover:text-white ring-1 ring-inset ring-gray-800 hover:bg-gray-800 focus:z-20 focus:outline-offset-0 transition-colors duration-200"
             >
               <ChevronDoubleLeftIcon className="h-5 w-5" aria-hidden="true" />
             </Link>
             <Link
               href={`${path}/${Math.max(1, currentPage - 1)}`}
-              className="relative inline-flex items-center px-2 py-2 text-gray-400 ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus:z-20 focus:outline-offset-0"
+              className="relative inline-flex items-center px-2 py-2 text-gray-800 hover:text-white ring-1 ring-inset ring-gray-800 hover:bg-gray-800 focus:z-20 focus:outline-offset-0 transition-colors duration-200"
             >
               <span className="sr-only">Previous</span>
               <ChevronLeftIcon className="h-5 w-5" aria-hidden="true" />
@@ -67,9 +67,9 @@ const Pagination = ({
                   href={`${path}/${page}`}
                   key={i}
                   className={
-                  currentPage == page
-                    ? "relative z-10 inline-flex items-center bg-indigo-600 px-4 py-2 text-sm font-semibold text-white focus:z-20 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-                    : "relative inline-flex items-center px-4 py-2 text-sm font-semibold text-gray-900 ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus:z-20 focus:outline-offset-0"
+                    currentPage == page
+                      ? "relative z-10 inline-flex items-center bg-gray-800 px-4 py-2 text-sm font-semibold text-white focus:z-20 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-gray-600"
+                      : "relative inline-flex items-center px-4 py-2 text-sm font-semibold transition-colors duration-200 text-gray-800 hover:text-white ring-1 ring-inset ring-gray-800 hover:bg-gray-800 focus:z-20 focus:outline-offset-0"
                   }
                 >
                   {page}
@@ -78,14 +78,14 @@ const Pagination = ({
             })}
             <Link
               href={`${path}/${Math.min(totalPage, currentPage + 1)}`}
-              className="relative inline-flex items-center px-2 py-2 text-gray-400 ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus:z-20 focus:outline-offset-0"
+              className="relative inline-flex items-center px-2 py-2 text-gray-800 hover:text-white ring-1 ring-inset ring-gray-800 hover:bg-gray-800 focus:z-20 focus:outline-offset-0 transition-colors duration-200"
             >
               <span className="sr-only">Next</span>
               <ChevronRightIcon className="h-5 w-5" aria-hidden="true" />
             </Link>
             <Link
               href={`${path}/${totalPage}`}
-              className="relative inline-flex items-center rounded-r-md px-2 py-2 text-gray-400 ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus:z-20 focus:outline-offset-0"
+              className="relative inline-flex items-center px-2 py-2 text-gray-800 hover:text-white ring-1 ring-inset ring-gray-800 hover:bg-gray-800 focus:z-20 focus:outline-offset-0 transition-colors duration-200"
             >
               <ChevronDoubleRightIcon className="h-5 w-5" aria-hidden="true" />
             </Link>
